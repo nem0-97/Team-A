09782398bbb5b0f4a687177317327886243a2f0b
Christian <Christian@guzzy.dk>
2019-09-25 21:15:12 -0700

    adding routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 import RestSignup from './RestaurantPages/RestSignup';
 import * as serviceWorker from './serviceWorker';
+
+function Index() {
+    return <h2>Home</h2>;
+}
+
+function Restaurant() {
+    return <RestSignup />
+}
+
+function Customer() {
+    return <h2>Customer Signup page</h2>;
+}
+
 
 
 class App extends Component{
@@ -10,8 +24,27 @@ class App extends Component{
         return(
             <div className="App">
 
-                <RestSignup />
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/Restaurant/">Restaurant</Link>
+                                </li>
+                                <li>
+                                    <Link to="/Customer/">Customer</Link>
+                                </li>
+                            </ul>
+                        </nav>
 
+                        <Route path="/" exact component={Index} />
+                        <Route path="/Restaurant/" component={Restaurant} />
+                        <Route path="/Customer/" component={Customer} />
+                    </div>
+                </Router>
             </div>
             
         )
