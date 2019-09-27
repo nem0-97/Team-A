94b4650bd967354984713190d08929e6574f5d9a
Nicholas Kim <nick.96.kim.y@gmail.com>
2019-09-26 18:26:19 -0700

    added random elements

--- frontend/src/PageComponents/RestCard.js
+++ frontend/src/PageComponents/RestCard.js
@@ -13,7 +13,12 @@ import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import tileData from './tileData';
-
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Badge from '@material-ui/core/Badge';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import InfoIcon from '@material-ui/icons/Info';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -28,12 +33,20 @@ const useStyles = makeStyles(theme => ({
       // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
       transform: 'translateZ(0)',
     },
+    gridList2: {
+        width: 1000,
+    },
     title: {
       color: theme.palette.primary.light,
     },
     titleBar: {
       background:
         'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    avatar: {
+        position: "relative",
+        left: 1500,
+        right: 200,
     },
   }));
 
@@ -42,26 +55,62 @@ export default function RestCard(){
     const classes = useStyles();
 
     return(
-        <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5}>
-                {tileData.map(tile => (
-                    <GridListTile key={tile.img}>
-                        <img src={tile.img} alt={tile.title} />
-                        <GridListTileBar
-                            title={tile.title}
-                            classes={{
-                                root: classes.titleBar,
-                                title: classes.title,
-                        }}
-                        actionIcon={
-                            <IconButton aria-label={`star ${tile.title}`}>
-                                <AddShoppingCartIcon className={classes.title} />
-                            </IconButton>
-                        }
-                    />
+        <div>
+            <div>
+                <Grid className={classes.avatar}>
+                    <IconButton>
+                        <Avatar>Hi</Avatar>
+                    </IconButton>    
+                    <IconButton>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartIcon />
+                        </Badge>
+                    </IconButton>
+                </Grid>
+            </div>
+
+            <div className={classes.root}>
+                <GridList className={classes.gridList} cols={2.5}>
+                    {tileData.map(tile => (
+                        <GridListTile key={tile.img}>
+                            <img src={tile.img} alt={tile.title} />
+                            <GridListTileBar
+                                title={tile.title}
+                                classes={{
+                                    root: classes.titleBar,
+                                    title: classes.title,
+                            }}
+                            actionIcon={
+                                <IconButton aria-label={`star ${tile.title}`}>
+                                    <AddShoppingCartIcon className={classes.title} />
+                                </IconButton>
+                            }
+                        />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
+
+            <div className={classes.root}>
+                <GridList cellHeight={150} className={classes.gridList2}>
+                    <GridListTile cols={4}>
+                        <ListSubheader component="div">RESTAURANTS</ListSubheader>
                     </GridListTile>
-                ))}
-            </GridList>
+                    {tileData.map(tile => (
+                        <GridListTile key={tile.img}>
+                            <img src={tile.img} alt={tile.title} />
+                            <GridListTileBar
+                            title={tile.title}
+                            actionIcon={
+                                <IconButton className={classes.icon}>
+                                <AddShoppingCartIcon />
+                                </IconButton>
+                            }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
         </div>
     );
 }
