32c069b8e255d62d7e1c9038ea7c17d124a43437
neoston101 <neoston10@gmail.com>
2019-09-27 12:58:42 -0700

    cleeaned up backend app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const MongoDB=require("./MongoDB")//import database helpers
 /** Express Setup Begin*/
 const https = require('https')
 const fs = require('fs')
-
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -12,10 +11,11 @@ const port = 3000
 app.use(cors())
 
 const jsonParser= express.json();
-
 /** Express Setup End*/
 
-/**RESTAURAUNT INFO API ROUTES*/
+/** API ROUTES*/
+
+/**RESTAURAUNT*/
 //GET
 app.get('/api/v1/rest', function (req, res) { //get a restaurant by name?
     console.log(req.query); //query parameters
@@ -48,80 +48,54 @@ app.delete('/api/v1/rest', jsonParser, function (req, res) { //remove a restaura
 })
 
 
-/**CUSTOMER INFO API ROUTES*/
+/**CUSTOMER*/
 //GET
-app.get('/api/v1/cust', function (req, res) { //get a restaurant by name?
-    console.log(req.query); //query parameters
-    if (Object.keys(req.query).length==0){//return all restaurants
-        console.log("All rstaurants will be queried");
-    }else{
-        //cycle through req.query 
-    }
-    console.log("/rest got get req"); //query database for reestaurants with matching name (req.params.name)
-    res.send({"message":'GET request to the homepage'}); //send list of results
+app.get('/api/v1/cust', function (req, res) {
+    
 })
   
 //POST
-app.post('/api/v1/cust', jsonParser, function (req, res) { //Add a new restaurant into database
-    console.log(req);
-    console.log("/rest got post req");
-    console.log(req.body); //add body to database, maybee have some extra logic here to build db formatted json with info sent over
-    res.send({"message":'POST request to the homepage, restaurant added to database'});
+app.post('/api/v1/cust', jsonParser, function (req, res) {
+
 })
 
 //PUT
-app.put('/api/v1/cust', jsonParser, function (req, res) { //Update given property of a restaurant with given value
-    console.log(req);
-    console.log("/rest got put req");
-    console.log(req.body); // retreive rest name, fields to update and new values theen update in DB
-    res.send({"message":'PUT request to the homepage, restaurant fields updatd'});
+app.put('/api/v1/cust', jsonParser, function (req, res) {
+ 
 })
 
 //DELETE
-app.delete('/api/v1/cust', jsonParser, function (req, res) { //remove a restaurant from database by name
-    console.log(req);
-    console.log("/rest got delete req");
-    console.log(req.body); //remove restaurant from database
-    res.send({"message":'DELETE request to the homepage'});
+app.delete('/api/v1/cust', jsonParser, function (req, res) {
+
 })
 
 
 /**ORDER API ROUTES*/
 //GET
-app.get('/api/v1/order', function (req, res) { //get a restaurant by name?
-    console.log(req.query); //query parameters
-    if (Object.keys(req.query).length==0){//return all restaurants
-        console.log("All rstaurants will be queried");
-    }else{
-        //cycle through req.query 
-    }
-    console.log("/rest got get req"); //query database for reestaurants with matching name (req.params.name)
-    res.send({"message":'GET request to the homepage'}); //send list of results
+app.get('/api/v1/order', function (req, res) { 
+    
 })
   
 //POST
-app.post('/api/v1/order', jsonParser, function (req, res) { //Add a new restaurant into database
-    console.log(req);
-    console.log("/rest got post req");
-    console.log(req.body); //add body to database, maybee have some extra logic here to build db formatted json with info sent over
-    res.send({"message":'POST request to the homepage, restaurant added to database'});
+app.post('/api/v1/order', jsonParser, function (req, res) { 
+    
 })
 
 //PUT
-app.put('/api/v1/order', jsonParser, function (req, res) { //Update given property of a restaurant with given value
-    console.log(req);
-    console.log("/rest got put req");
-    console.log(req.body); // retreive rest name, fields to update and new values theen update in DB
-    res.send({"message":'PUT request to the homepage, restaurant fields updatd'});
+app.put('/api/v1/order', jsonParser, function (req, res) { 
+
 })
 
 //DELETE
-app.delete('/api/v1/order', jsonParser, function (req, res) { //remove a restaurant from database by name
-    console.log(req);
-    console.log("/rest got delete req");
-    console.log(req.body); //remove restaurant from database
-    res.send({"message":'DELETE request to the homepage'});
+app.delete('/api/v1/order', jsonParser, function (req, res) { 
+    
 })
+
+
+
+
+
+
 
 //start server
 https.createServer({
