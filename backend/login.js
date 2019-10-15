3b10da0e6c9dab1e9c10942a206287e288f265a4
neoston101 <neoston10@gmail.com>
2019-10-14 17:37:55 -0700

    enabled hashing of passwords and check at login, also able to login as customer or rest

--- backend/login.js
+++ backend/login.js
@@ -1,11 +1,10 @@
 /**Login */
-//const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 
 exports.hashPass = function (pass){
     return bcrypt.hashSync(pass, 10);// 10 rounds
 }
 
 exports.passIsHash = function(pass, hash){
-    return pass==hash;//temporary
-    //return bcrypt.compareSync(pass,hash);//works but w have not starteed hashing passwords before putting theem into thee database yet
+    return bcrypt.compareSync(pass,hash);//works but w have not starteed hashing passwords before putting theem into thee database yet
 }
