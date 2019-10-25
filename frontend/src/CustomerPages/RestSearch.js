e6355a28691546f1f95083340d8f8050086c3ac6
Christian Henriksen <Christian@Christians-MacBook-Pro.local>
2019-10-24 19:20:43 -0700

    adding distance functionality, neoman might help with getting it rendering

--- frontend/src/CustomerPages/RestSearch.js
+++ frontend/src/CustomerPages/RestSearch.js
@@ -3,72 +3,97 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import './restSearch.css';
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+let params = new URLSearchParams(window.location.search); 
+let lng = params.get('lng');
+let lat = params.get('lat');
+let restaurants = [];
+
 /*
-TODO: 
-    #Google maps dinstance matrix API:
-        #calculate distance between searchquery and restaurants - https://developers.google.com/maps/documentation/distance-matrix/start
-
+    This function will calculate the distance between to coordinates
+    We are getting one coordinate from the url param, and the other one we will fetch from the database.
+    @param lat1 = first latitude coordinate
+    @param lng1 = second longtitude coordinate
+    @ unit = if it should use metric or miles, K = metric default is miles
+    Credit to:  https://www.geodatasource.com/developers/javascript 
 */
-
-function getCoordinates(){
-   
-
+function calcDistance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		return dist;
+	}
 }
 
 class RestSearch extends React.Component {
     constructor(props) {
-        let params = new URLSearchParams(window.location.search)
         super(props);
-        this.state = {
-        /* gets the query param  */
-         lng: params.get('lng'),
-         lat: params.get('lat'),
-         data: [],
-        };
+        console.log("Latitude: "+lat);
+        console.log("Longtitude: "+lng);
         
-        console.log(params.get('lat'));
-        console.log(params.get('lng'));
-        
-    }
-    componentDidMount(){
-        getCoordinates();
-        
-        
-        //fetch('https://localhost:3000/api/v1/rest').then(response => response.json()).then(response1 => {this.setState({tileData: response1.results})});
-        //fetch('https://localhost:3000/api/v1/rest').then(response => response.json()).then(response1 => {this.setState({data: response1})});
-
-        /* FIXME: SHOULD NOT FETCH EVERYTHING
-                 RIGHT NOW IT's ALSO fetching password etc from the restaurant collection.
-        
-        */
         async function apiGetAll () {
             try {
               const resp = await fetch('https://localhost:3000/api/v1/rest')
-              
-              
               return resp.json();
             } catch (err) {
                  console.log(err)
               }
          }
          this.data = apiGetAll().then(function(data){
-            console.log(data.results);
-        });;
-
+            console.log(data.results.restInfo);
+            /*
+                 loop through all the fetched data, and see if the coordinates is is less than X away
+                 If it is, put it into restaurant array for later to be displayed
+            */
+            data.results.forEach(item => {
+                let lng2 = item.restinfo.lng;
+                let lat2 = item.restinfo.lat;
+                let distance = calcDistance(lat,lng,lat2,lng2,"m");
+                if(distance <= 5){ //If the restaurant is less than 5km away, we will probably have to change this later for the user to change the distance
+                    console.log(item.restinfo.restName + " is: " + distance.toFixed(3) + " miles away from (" + lat + "," + lng + ")");
+                    restaurants.push(item);
+                    console.log(restaurants);
+                }
+                else {
+                    console.log(item.restinfo.restName + " is not within the target range");
+                }
+               
+            });
+        });
     }
     
+    
     render() {
+        /* 
+        FIXME: Neoman can you fix why it is not rendering my restaurants array?
         
+        */
             return (   
                 <Container component="main" maxWidth="md">
                 <Paper className="containerPadding">
               <Typography variant="h3" className="text-center">
                   Restaurants located on this location:
               </Typography>
-              <p className="coordinates">
-                  lng: {this.state.lng} <br/>
-                  lat: {this.state.lat}
-              </p>
+              
+              <ol>
+      {restaurants.map(restaurants => <li>{restaurants.restinfo.restName}</li>)}
+    </ol>
                 
               </Paper>
               </Container>
