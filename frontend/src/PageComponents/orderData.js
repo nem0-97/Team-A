1c98f9f71d7a96b9c0e2df5287073d72acabc32a
Mannat Kaur <mannatk@gmail.com>
2019-11-04 19:58:21 -0800

    fixed checkout bug

---
+++ frontend/src/PageComponents/orderData.js
@@ -0,0 +1,12 @@
+const orderData = [
+    {
+      img: 'food.png',
+      title: 'Meal',
+    }, 
+    {
+      img: 'food.png',
+      title: 'Meal',
+    }, 
+  ];
+  
+  export default orderData;
